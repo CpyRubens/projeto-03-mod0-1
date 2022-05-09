c4e42508749d22f6040484fec2f42b45050a3d4e
CpyRubens <joaorubens2500@hotmail.com>
2022-05-09 19:57:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 const prompt = require("prompt-sync")();
 console.clear();
 
-function randomEvent(){
-let random100 = Math.floor(Math.random() * 100);
-console.log(random100)
-if (jogadorStatuses.Dinheiro >= 7 && random100 >= 75){
-  console.log("Você acumulou muito dinheiro, o que atraiu sequestradores até você!")
-  console.log("Você pode pagar 2 de dinheiro ou tentar fugir(perigoso)")
-  escapar = prompt("pagar ou fugir?")
-  while(escapar.toLowerCase() != "pagar" && escapar.toLowerCase() != "fugir"){
-    escapar = prompt("PAGAR OU FUGIR???")
-  }
-  if (escapar == "pagar"){
-    console.log("Você pagar a quantia solicitada(-2 de dinheiro) e é libertado para continuar sua jornada)")
-    jogadorStatuses.Dinheiro -= 2;
-  }
-  else{
-    let randomdeath = Math.floor(Math.random() * 100);
-    if(randomdeath >= 50){
-      console.log("Você foi morto ao tentar fugir. Fim de jogo!")
-    }
-    else{
-      console.log("Você consegue escapar com sucesso dos sequestradores e volta a sua jornada!")
-    }
-  }
-}
-}
+// variável de controle de tempo e função de controle de tempo
+let semanasPercorridas = 0;
 
-//funções
 function passarTempo() {
-  mesesPercorridos = mesesPercorridos + 1;
+  semanasPercorridas = semanasPercorridas + 1;
   console.log("Se passou mais um mês");
-  return mesesPercorridos;
+  return semanasPercorridas;
 }
 
+//objeto principal referente aos status e funções de ações do jogador
 let jogadorStatuses = {
   Respeito: 5,
   Dinheiro: 5,
@@ -46,7 +23,7 @@ let jogadorStatuses = {
     this.Vigor += 1;
     passarTempo();
     console.log(
-      "Você doa parte do seu dinheiro para instituições de caridade, aumentando a sua moral social e se revigorando\n"
+      "Você doa parte do seu dinheiro para instituições de caridade, aumentando a sua moral social e se revigorando\nDinheiro -2\nVigor +1\nRespeito +2"
     );
   },
   mercadoNegro: function () {
@@ -55,14 +32,14 @@ let jogadorStatuses = {
     this.Legalidade -= 2;
     passarTempo();
     console.log(
-      "Você investe no mercado ilegal obtendo excelente retorno financeiro, porém sua reputação despenca e justiça fica de olho em você\n"
+      "Você investe no mercado ilegal obtendo excelente retorno financeiro, porém sua reputação cai e a justiça fica de olho em você\nDinheiro +2\nRespeito -1\nLegalidade -2"
     );
   },
   mercadoAcoes: function () {
     this.Dinheiro += 1;
     this.Vigor -= 1;
     passarTempo();
-    console.log("Você investe no mercado de ações obtendo um retorno legal\n");
+    console.log("Você investe no mercado de ações e trabalha obtendo um retorno legal\nDinheiro +1 \nVigor -1");
   },
 
   sonegacao: function () {
@@ -76,17 +53,73 @@ let jogadorStatuses = {
   },
 };
 
-mesesPercorridos = 0;
+//função de evento aleatório dentro do jogo(sobre determinada situação)
+function randomEvent() {
+  let random100 = Math.floor(Math.random() * 100);
+  console.log(random100);
+  if (jogadorStatuses.Dinheiro >= 7 && random100 >= 75) {
+    console.log(
+      "Você acumulou muito dinheiro, o que atraiu sequestradores até você!"
+    );
+    console.log("Você pode pagar 2 de dinheiro ou tentar fugir(perigoso)");
+    escapar = prompt("pagar ou fugir?");
+    while (
+      escapar.toLowerCase() != "pagar" &&
+      escapar.toLowerCase() != "fugir"
+    ) {
+      escapar = prompt("PAGAR OU FUGIR???");
+    }
+    if (escapar == "pagar") {
+      console.log(
+        "Você pagar a quantia solicitada(-2 de dinheiro) e é libertado para continuar sua jornada)"
+      );
+      jogadorStatuses.Dinheiro -= 2;
+    } else {
+      let randomdeath = Math.floor(Math.random() * 100);
+      if (randomdeath >= 50) {
+        console.log("Você foi morto ao tentar fugir. Fim de jogo!");
+      } else {
+        console.log(
+          "Você consegue escapar com sucesso dos sequestradores e volta a sua jornada!"
+        );
+      }
+    }
+  }
+}
+function imposto(){
+  if (semanasPercorridas > 0 && semanasPercorridas % 4 == 0){
+    console.log("É hora de pagar os impostos, mas você também pode sonegar este evento")
+    let imposto = prompt("Pagar ou Sonegar?")
+    while (imposto.toLocaleLowerCase() != "pagar" && imposto.toLocaleLowerCase() != "sonegar"){
+      imposto = prompt("PAGAR ou SONEGAR?")
+    }
+    if (imposto.toLocaleLowerCase() == "pagar" ){
+      console.log("Você paga seus impostos como um cidadão normal. \nDinheiro -1")
+      jogadorStatuses.Dinheiro -= 1;
+    }
+    else{
+      console.log("Você sonega os impostos desse mês com tranquilidade e acumula mais dinheiro. \nDinheiro +1 \nLegalidade -1")
+      jogadorStatuses.Dinheiro += 1;
+      jogadorStatuses.Legalidade -= 1;
+    }
+  }
+}
+
 
 console.log(
-  "Bem vindo a sua jornada em busca do sucesso, para isso você vai ter que saber bem onde utilzar o seu dinheiro. Todas as saus ações tem consequências, cuidado!\nOnde deseja investir o dinheiro essa semana?\n"
+  "Bem vindo a sua jornada em busca do sucesso, para isso você vai ter que saber bem onde utilzar o seu dinheiro. Todas as saus ações tem consequências!\n Para vencer chegue a 10 em dinheiro ou respeito, porém se qualquer atributo chegara a 0 você perde, cuidado!\nOnde deseja investir o dinheiro essa semana?\n"
 );
 
 //laço
 while (1) {
+  console.log(`-----------------------------------------------------------------------------------------------------------------
+[1] Doar dinheiro para a instituições de caridade: irá te conceder muito respeito social e te dará energias para continuar.
+[2] Investir dinheiro no mercado negro: te rende muito dinheiro, porém destrói sua reputação e pode te colocar atrás das grades.
+[3] Investir/trabalhar: te rende dinheiro honesto porém cansa aos poucos\n
+---------------------------------------------------------------------------------------------------------------------------------`)
   decisao = +prompt("Escolha como irá agir durante este mês");
-  while ( isNaN(decisao) && decisao >= 6){
-    decisao =+prompt("Escolha um numeral válido!")
+  while (isNaN(decisao) && decisao >= 6) {
+    decisao = +prompt("Escolha um numeral válido!");
   }
   if (decisao == 1) {
     jogadorStatuses.caridade();
@@ -100,13 +133,18 @@ while (1) {
     break;
   }
 
-  console.log( //usa um For aqui
-  "Tempo percorrido",mesesPercorridos,
-  "Dinheiro:",jogadorStatuses.Dinheiro,
-  "Legalidade:",jogadorStatuses.Legalidade,
-  "Respeito:",jogadorStatuses.Respeito,
-  "Vigor:",jogadorStatuses.Vigor
-)
+  console.log(
+    "Tempo percorrido",
+    semanasPercorridas,
+    "Dinheiro:",
+    jogadorStatuses.Dinheiro,
+    "Legalidade:",
+    jogadorStatuses.Legalidade,
+    "Respeito:",
+    jogadorStatuses.Respeito,
+    "Vigor:",
+    jogadorStatuses.Vigor
+  );
 
   // verificador de status dinheiro
   if (jogadorStatuses.Dinheiro <= 0) {
@@ -157,7 +195,6 @@ while (1) {
     );
     break;
   }
-  randomEvent()
+  randomEvent();
+  imposto();
 }
-
-
