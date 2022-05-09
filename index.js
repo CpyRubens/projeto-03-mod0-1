dc03df98f362201111aaa473e8d18eb88a51598f
CpyRubens <joaorubens2500@hotmail.com>
2022-05-09 15:57:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const prompt = require("prompt-sync")();
 console.clear();
-("John Travolta/Enriquecer/ fazer o certo ou o errado/ na possibilidade de ser preso, morrer ou vencer, a cada ação");
-jogadorStatuses = { Respeito: 5, Dinheiro: 5, Legalidade: 5, Vigor: 5 };
 
-mesesPercorridos = 0;
+function randomEvent(){
+let random100 = Math.floor(Math.random() * 100);
+console.log(random100)
+if (jogadorStatuses.Dinheiro >= 7 && random100 >= 75){
+  console.log("Você acumulou muito dinheiro, o que atraiu sequestradores até você!")
+  console.log("Você pode pagar 2 de dinheiro ou tentar fugir(perigoso)")
+  escapar = prompt("pagar ou fugir?")
+  while(escapar.toLowerCase() != "pagar" && escapar.toLowerCase() != "fugir"){
+    escapar = prompt("PAGAR OU FUGIR???")
+  }
+  if (escapar == "pagar"){
+    console.log("Você pagar a quantia solicitada(-2 de dinheiro) e é libertado para continuar sua jornada)")
+    jogadorStatuses.Dinheiro -= 2;
+  }
+  else{
+    let randomdeath = Math.floor(Math.random() * 100);
+    if(randomdeath >= 50){
+      console.log("Você foi morto ao tentar fugir. Fim de jogo!")
+    }
+    else{
+      console.log("Você consegue escapar com sucesso dos sequestradores e volta a sua jornada!")
+    }
+  }
+}
+}
 
 //funções
 function passarTempo() {
@@ -12,57 +34,130 @@ function passarTempo() {
   return mesesPercorridos;
 }
 
-function caridade() {
-  jogadorStatuses.Dinheiro = jogadorStatuses.Dinheiro - 2;
-  jogadorStatuses.Respeito = jogadorStatuses.Respeito + 1;
-  passarTempo();
-}
-function mercadoNegro() {
-  jogadorStatuses.Dinheiro = jogadorStatuses.Dinheiro + 2;
-  jogadorStatuses.Respeito = jogadorStatuses.Respeito - 2;
-  jogadorStatuses.Legalidade = -2;
-  passarTempo();
-}
+let jogadorStatuses = {
+  Respeito: 5,
+  Dinheiro: 5,
+  Legalidade: 5,
+  Vigor: 5,
 
-function mercadoAcoes() {
-  jogadorStatuses.Dinheiro = jogadorStatuses.Dinheiro + 1;
-  jogadorStatuses.Vigor = jogadorStatuses.Vigor - 1;
-  passarTempo();
-}
+  caridade: function () {
+    this.Dinheiro -= 2;
+    this.Respeito += 2;
+    this.Vigor += 1;
+    passarTempo();
+    console.log(
+      "Você doa parte do seu dinheiro para instituições de caridade, aumentando a sua moral social e se revigorando\n"
+    );
+  },
+  mercadoNegro: function () {
+    this.Dinheiro += 2;
+    this.Respeito -= 1;
+    this.Legalidade -= 2;
+    passarTempo();
+    console.log(
+      "Você investe no mercado ilegal obtendo excelente retorno financeiro, porém sua reputação despenca e justiça fica de olho em você\n"
+    );
+  },
+  mercadoAcoes: function () {
+    this.Dinheiro += 1;
+    this.Vigor -= 1;
+    passarTempo();
+    console.log("Você investe no mercado de ações obtendo um retorno legal\n");
+  },
 
-function sonegacao() {
-  jogadorStatuses.Dinheiro = jogadorStatuses.Dinheiro + 2;
-  jogadorStatuses.Respeito = jogadorStatuses.Respeito - 1;
-  jogadorStatuses.Legalidade = jogadorStatuses.Legalidade - 1;
-  passarTempo();
-}
+  sonegacao: function () {
+    this.Dinheiro += 1;
+    this.Respeito -= 1;
+    this.Legalidade -= 1;
+    passarTempo();
+    console.log(
+      "Você dribla a fiscalização e não declara seus bens, fugindo das impostos e acumulando mais dinheiro, porém entrando na mira da Lei\n"
+    );
+  },
+};
+
+mesesPercorridos = 0;
+
 console.log(
-  "Bem vindo ao Mundo X\n Onde deseja investir o dinheiro essa semana?"
+  "Bem vindo a sua jornada em busca do sucesso, para isso você vai ter que saber bem onde utilzar o seu dinheiro. Todas as saus ações tem consequências, cuidado!\nOnde deseja investir o dinheiro essa semana?\n"
 );
 
+//laço
 while (1) {
   decisao = +prompt("Escolha como irá agir durante este mês");
+  while ( isNaN(decisao) && decisao >= 6){
+    decisao =+prompt("Escolha um numeral válido!")
+  }
   if (decisao == 1) {
-    caridade();
+    jogadorStatuses.caridade();
   } else if (decisao == 2) {
-    mercadoNegro();
+    jogadorStatuses.mercadoNegro();
   } else if (decisao == 3) {
-    mercadoAcoes();
+    jogadorStatuses.mercadoAcoes();
   } else if (decisao == 4) {
-    sonegacao();
+    jogadorStatuses.sonegacao();
   } else if (decisao == 5) {
     break;
   }
-  for (const property in jogadorStatuses) {
-    console.log(property + " = " + jogadorStatuses[property]);
+
+  console.log( //usa um For aqui
+  "Tempo percorrido",mesesPercorridos,
+  "Dinheiro:",jogadorStatuses.Dinheiro,
+  "Legalidade:",jogadorStatuses.Legalidade,
+  "Respeito:",jogadorStatuses.Respeito,
+  "Vigor:",jogadorStatuses.Vigor
+)
+
+  // verificador de status dinheiro
+  if (jogadorStatuses.Dinheiro <= 0) {
+    console.log(
+      "Você faliu, está devemdo muito dinheiro. É o fim da linha para você\n"
+    );
+    break;
+  } else if (
+    jogadorStatuses.Dinheiro >= 10 &&
+    jogadorStatuses.Vigor <= 0 &&
+    jogadorStatuses.Legalidade <= 0 &&
+    jogadorStatuses.Respeito <= 0
+  ) {
+    console.log(
+      "Parabéns, você atingiu dinheiro suficiente para viver bem o resto da sua vida! Parabéns\n"
+    );
+    break;
   }
-  
+  //verifcador de status respeito
+  if (jogadorStatuses.Respeito <= 0) {
+    console.log(
+      "Você se tornou a definição de escória social, suas atitudes te colocarm num pedestal negativo ireversível!\n"
+    );
+    break;
+  } else if (
+    jogadorStatuses.Dinheiro <= 0 &&
+    jogadorStatuses.Vigor <= 0 &&
+    jogadorStatuses.Legalidade <= 0 &&
+    jogadorStatuses.Respeito >= 10
+  ) {
+    console.log(
+      "Parabéns, você atingiu um respeito moral incrível, agora se tornando uma pessoa pública muito imporatante, todos tem muita consideração por você. Você venceu!\n "
+    );
+    break;
+  }
+  // verificador de status vigor
+  if (jogadorStatuses.Vigor <= 0) {
+    console.log(
+      "Você está sem motivação nenhuma para continuar e resolve procurar tratamento médico, melhor dar um tempo definitivo a essa jornada\n"
+    );
+    break;
+  }
+
+  //verificador de status legalidade
+  if (jogadorStatuses.Legalidade <= 0) {
+    console.log(
+      "Você se tornou um criminoso financeiro muito procurado, a polícia finalmente te pega e você perde tudo! Fim de jogo!.....\n"
+    );
+    break;
+  }
+  randomEvent()
 }
 
-console.log(
-  mesesPercorridos,
-  jogadorStatuses.Dinheiro,
-  jogadorStatuses.Legalidade,
-  jogadorStatuses.Respeito,
-  jogadorStatuses.Vigor
-);
+
